43e27952e06517113863a048fc8315bc57c84f4e
nawaaaaab <nawaaaaab@gmail.com>
2022-02-14 14:25:49 -0500

    Added server

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ classifier = pipeline("sentiment-analysis")
 
 app = Dash(__name__)
 
+server = app.server
+
 app.layout = html.Div([
     html.H1("Sentiment Analyzer Playground"),
     html.Div([
@@ -23,10 +25,10 @@ app.layout = html.Div([
 )
 def update_output_div(input_value):
     result = classifier(input_value)[0]
-    output_value = f"label: {result['label']}, with score: {round(result['score'], 4)}"
+    output_value = f"label: {result['label']}, "\
+                   f"with score: {round(result['score'], 4)}"
     return f'Output: {output_value}'
 
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
